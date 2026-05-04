04385e1ec7609e2e55874aab1c338e9e1190e8ce
slatro <slatro.eth@gmail.com>
2026-05-04 19:17:17 +0300

    deploy: manual github pages build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Using relative paths for maximum compatibility
 })
